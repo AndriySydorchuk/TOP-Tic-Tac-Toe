c12f27cadaf120a48b94ae30d9c5e49ff3f0e404
Andriy Sydorchuk <programmers130822@gmail.com>
2026-02-08 11:17:49 +0200

    Refactor code structure and add randomMove function for handling player moves

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
+const Game = (function () {
+    let player;
+    let computer;
+    const init = function () {
+        console.log('Game init call');
+        player = createPlayer('Furman', 'X', randomMove);
+        computer = createPlayer('Computer', 'O', randomMove);
+
+        console.log(`Player1: ${player.getName()}, ${player.getSign()}`);
+        console.log(`Player2: ${computer.getName()}, ${computer.getSign()}`);
+    };
+    const play = function () {
+    };
+
+    return { init, play };
+})();
+
 const Gameboard = (function () {
     const board = [
-        ['X', 'X', 'X'],
-        ['O', 'X', 'O'],
-        ['X', 'O', 'X']
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']
     ];
     const display = function () {
         for (const line of board) {
             console.log(`${line}\n`);
         }
+    };
+    const isEmptyCell = function (row, col) {
+        if (board[row][col] === '-') {
+            return true;
+        }
+        return false;
+    };
+    const setCell = function (row, col, sign) {
+        board[row][col] = sign;
     }
 
-    return { display }
+    return { display, isEmptyCell, setCell }
 })();
 
 function createPlayer(name, sign) {
@@ -33,5 +59,20 @@ function createPlayer(name, sign) {
     }
 }
 
-const player = createPlayer('Furman', 'X');
-const computer = createPlayer('Computer', 'O');
+function randomMove(sign) {
+    const maxLineLength = 3; // 0..2
+    let rowIndex;
+    let colIndex;
+
+    do {
+        rowIndex = Math.floor(Math.random() * maxLineLength);
+        colIndex = Math.floor(Math.random() * maxLineLength);
+
+        console.log(`Generated coordinates: [${rowIndex}, ${colIndex}]`)
+
+    } while (!Gameboard.isEmptyCell(rowIndex, colIndex));
+
+    Gameboard.setMove(rowIndex, colIndex, sign);
+
+    console.log(`Cell [${rowIndex}, ${colIndex}] is set to ${sign}`);
+}
