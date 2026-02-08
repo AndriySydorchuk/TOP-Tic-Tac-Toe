46d8fc419e80d35b4d175085516d6b21d5e7336f
Andriy Sydorchuk <programmers130822@gmail.com>
2026-02-08 19:36:44 +0200

    Add play function to the Game that will handle flow of the game

--- script.js
+++ script.js
@@ -5,12 +5,64 @@ const Game = (function () {
         console.log('Game init call');
         player = createPlayer('Furman', 'X', randomMove);
         computer = createPlayer('Computer', 'O', randomMove);
-
-        console.log(`Player1: ${player.getName()}, ${player.getSign()}`);
-        console.log(`Player2: ${computer.getName()}, ${computer.getSign()}`);
     };
     const play = function () {
+        let win = false;
+
+        do {
+            Gameboard.display();
+
+            player.move();
+            Gameboard.display();
+
+            win = checkWin();
+
+            if (!win) {
+                computer.move();
+                Gameboard.display();
+            }
+
+            win = checkWin();
+
+        } while (!win);
     };
+    const checkWin = function () {
+        let win = false;
+
+        for (let i = 0; i < Gameboard.winCombinations.length; i++) {
+            //get combination
+            const winCondition = Gameboard.winCombinations[i];
+
+            //get cells
+            const [cell1, cell2, cell3] = winCondition;
+
+            //check if all these cells are not empty
+            if (
+                Gameboard.isEmptyCell(cell1[0], cell1[1]) ||
+                Gameboard.isEmptyCell(cell2[0], cell2[1]) ||
+                Gameboard.isEmptyCell(cell3[0], cell3[1])
+            ) {
+                continue;
+            }
+
+            //check if they're the same
+            const val1 = Gameboard.getCell(cell1[0], cell1[1]);
+            const val2 = Gameboard.getCell(cell2[0], cell2[1]);
+            const val3 = Gameboard.getCell(cell3[0], cell3[1]);
+
+            if (val1 === val2 && val2 === val3 && val1 !== '-') {
+                return true;
+            }
+
+        }
+
+        if (win) return true;
+
+        //tie
+        if (Gameboard.isFull()) return true;
+
+        return false;
+    }
 
     return { init, play };
 })();
@@ -21,6 +73,19 @@ const Gameboard = (function () {
         ['-', '-', '-'],
         ['-', '-', '-']
     ];
+    const winCombinations = [
+        [[0, 0], [0, 1], [0, 2]],
+        [[1, 0], [1, 1], [1, 2]],
+        [[2, 0], [2, 1], [2, 2]],
+
+        [[0, 0], [1, 0], [2, 0]],
+        [[0, 1], [1, 1], [2, 1]],
+        [[0, 2], [1, 2], [2, 2]],
+
+        [[0, 0], [1, 1], [2, 2]],
+        [[0, 2], [1, 1], [2, 0]],
+    ];
+
     const display = function () {
         for (const line of board) {
             console.log(`${line}\n`);
@@ -34,12 +99,18 @@ const Gameboard = (function () {
     };
     const setCell = function (row, col, sign) {
         board[row][col] = sign;
-    }
+    };
+    const getCell = function (row, col) {
+        return board[row][col];
+    };
+    const isFull = function () {
+        return board.flat().every(cell => cell !== '-');
+    };
 
-    return { display, isEmptyCell, setCell }
+    return { display, isEmptyCell, setCell, getCell, winCombinations, isFull }
 })();
 
-function createPlayer(name, sign) {
+function createPlayer(name, sign, moveFunc) {
     return {
         getName() {
             return name;
@@ -53,26 +124,25 @@ function createPlayer(name, sign) {
         setSign(newSign) {
             sign = newSign;
         },
-        move(func) {
-            func();
+        move() {
+            moveFunc(sign);
         }
     }
 }
 
 function randomMove(sign) {
     const maxLineLength = 3; // 0..2
-    let rowIndex;
-    let colIndex;
+    let row;
+    let col;
 
     do {
-        rowIndex = Math.floor(Math.random() * maxLineLength);
-        colIndex = Math.floor(Math.random() * maxLineLength);
+        row = Math.floor(Math.random() * maxLineLength);
+        col = Math.floor(Math.random() * maxLineLength);
 
-        console.log(`Generated coordinates: [${rowIndex}, ${colIndex}]`)
+    } while (!Gameboard.isEmptyCell(row, col));
 
-    } while (!Gameboard.isEmptyCell(rowIndex, colIndex));
-
-    Gameboard.setMove(rowIndex, colIndex, sign);
-
-    console.log(`Cell [${rowIndex}, ${colIndex}] is set to ${sign}`);
+    Gameboard.setCell(row, col, sign);
 }
+
+Game.init();
+Game.play();
